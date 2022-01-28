688aa3c8b19dde3a2dab654cf67917a04d5cc994
Mikelis Asnins <masnins@meteomatics.com>
2022-01-28 14:35:13 +0100

    Fix sourcemap and build warnings

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
   input: './src/index.ts',
@@ -16,6 +17,7 @@ export default {
   ],
   plugins: [
     typescript({ declaration: false, module: 'ES6' }),
+    sourcemaps(),
     resolve(),
     commonjs(),
     terser(),
